e30f7500fb1136e143124d0d609cffe743a5ee9e
rootmentsdev <rootmentsit@gmail.com>
2025-06-09 12:36:28 +0530

    changes in api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -14,7 +14,7 @@
 
 
 const baseUrl = {
-     baseUrl: 'http://localhost:7000/'/// https://lms-1-lavs.onrender.com 'http://3.110.163.189:7000/' //'https://lms-ildn.onrender.com/' http://localhost:7000/ https://lms-test-mfec.onrender.com
+     baseUrl: 'https://rootfin-testenv-3.onrender.com/'/// https://lms-1-lavs.onrender.com 'http://3.110.163.189:7000/' //'https://lms-ildn.onrender.com/' http://localhost:7000/ https://lms-test-mfec.onrender.com
 }
 export default baseUrl
 
